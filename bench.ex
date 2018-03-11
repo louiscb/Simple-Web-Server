a673bcc414ec08205be6bc693f875c7f8315890a
louiscb <louiscb@kth.se>
2018-03-11 23:53:42 +0100

    Made bench concurrent

--- bench.ex
+++ bench.ex
@@ -1,25 +1,31 @@
 defmodule Bench do
-  @number_requests 100
-
-  def bench(host, port) do
+  def bench(host, port, number_requests) do
     start = Time.utc_now()
-    run(@number_requests, host, port)
-    finish = Time.utc_now()
-    diff = Time.diff(finish, start, :millisecond)
-    IO.puts("Benchmark: #{@number_requests} requests in #{diff} ms")
+    mother = self()
+    run(number_requests, host, port, mother)
+
+    receive do
+      :last_request ->
+        finish = Time.utc_now()
+        diff = Time.diff(finish, start, :millisecond)
+        IO.puts("Benchmark: #{number_requests} requests in #{diff} ms")
+    end
   end
 
-  defp run(0, _host, _port), do: :ok
-  defp run(n, host, port) do
-    request(host, port)
-    run(n-1, host, port)
+  defp run(0, _, _, _), do: :ok
+  defp run(n, host, port, mother) do
+    spawn(fn()->request(n, host, port, mother) end)
+    run(n-1, host, port, mother)
   end
 
-  defp request(host, port) do
+  defp request(n, host, port, mother) do
     opt = [:list, active: false, reuseaddr: true]
     {:ok, server} = :gen_tcp.connect(host, port, opt)
     :gen_tcp.send(server, HTTP.get(host))
     {:ok, _reply} = :gen_tcp.recv(server, 0)
     :gen_tcp.close(server)
+    if n == 1 do
+      send(mother, :last_request)
+    end
   end
 end
